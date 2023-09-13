1f76ab26d3d2a75c96f2b2dd6ef32b45d65705ab
Max.l <a0958057936@gmail.com>
2023-09-13 18:07:50 +0800

    feature: zero ssl

--- main.py
+++ main.py
@@ -8,9 +8,24 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    try:
+        domain = 'test.hqsmaxtest.online'
+        current_file_path = os.path.abspath(__file__)
+        project_path = os.path.dirname(os.path.dirname(current_file_path))
+        temp_ssl_path = os.path.join(project_path, 'zero_ssl', 'module', 'ssl', 'temp_ssl')
+        full_path = os.path.join(temp_ssl_path, domain)
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
+        os.system(f'sudo openssl req -new -newkey rsa:2048 -nodes -keyout {full_path}/privkey.pem -out {full_path}/csr.pem -subj "/CN={domain}"')
+        os.system(f'sudo ~/.acme.sh/acme.sh --issue --webroot {full_path} -d {domain} --csr {full_path}/csr.pem --fullchainpath {full_path}/fullchain.pem --keypath {full_path}/privkey.pem')
+    except Exception as e:
+        print(e)
 
 
 @app.get("/.well-known/acme-challenge/{challenge_route}")
-async def get_certificate():
-    return "success"
+async def get_certificate(challenge_route: str, response: Response):
+    full_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'zero_ssl', 'module', 'ssl', 'temp_ssl', 'test.hqsmaxtest.online')
+    with open(os.path.join(full_path, challenge_route), 'r') as f:
+        response.body = f.read()
+    response.headers['Content-Type'] = 'text/plain'
+    return response
