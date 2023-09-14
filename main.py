6c7cefbe697c409d9d760179719043ac067a1469
Max.l <a0958057936@gmail.com>
2023-09-14 10:10:54 +0800

    feature: zero ssl

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ async def domain():
 
 @app.get("/.well-known/acme-challenge/{challenge_route}")
 async def get_certificate(challenge_route: str, response: Response):
-    # 暫停五秒
-    time.sleep(5)
+    print(challenge_route)
     full_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'cname_ssl_v7', 'module', 'ssl', 'temp_ssl', 'test.hqsmaxtest.online')
+    print(full_path)
     with open(os.path.join(full_path, '.well-known', 'acme-challenge', challenge_route), 'r') as f:
         response.body = f.read()
     response.headers['Content-Type'] = 'text/plain'
