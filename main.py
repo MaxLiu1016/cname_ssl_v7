34ff8312235f69ec4a214203c3159c669d640c6c
Max.l <a0958057936@gmail.com>
2023-09-14 11:08:20 +0800

    feature: zero ssl

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ async def domain():
         if not os.path.exists(full_path):
             os.makedirs(full_path)
         await run_command(f'sudo openssl req -new -newkey rsa:2048 -nodes -keyout {full_path}/privkey.pem -out {full_path}/csr.pem -subj "/CN={domain}"')
-        await run_command(f'sudo chmod -R 777 {full_path}')
-        await run_command(f'acme.sh --issue --webroot {full_path} -d {domain} --csr {full_path}/csr.pem --fullchainpath {full_path}/fullchain.pem --keypath {full_path}/privkey.pem --force --debug')
+        print(f'sudo openssl req -new -newkey rsa:2048 -nodes -keyout {full_path}/privkey.pem -out {full_path}/csr.pem -subj "/CN={domain}"')
+        # await run_command(f'sudo chmod -R 777 {full_path}')
+        # await run_command(f'acme.sh --issue --webroot {full_path} -d {domain} --csr {full_path}/csr.pem --fullchainpath {full_path}/fullchain.pem --keypath {full_path}/privkey.pem --force --debug')
         return {"message": "success"}
     except Exception as e:
         print(e)
